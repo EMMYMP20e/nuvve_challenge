47584ebee079cee44f62159507cbb563d5892159
emmy_ <emmy.mp.98e@gmail.com>
2023-03-30 15:38:09 -0600

    Initializer

---
+++ src/test/java/com/nuvve/challenge/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nuvve.challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
